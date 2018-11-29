ee2bcb5f41ff51d45db60caa580737f99b32afaa
takacchi <uchiha.takahiro@gmail.com>
2018-11-29 16:24:24 +0100

    Implemented Validations for blogs, skills and portfolios item

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -12,7 +12,7 @@ class PortfoliosController < ApplicationController
 
     respond_to do |format|
       if @portfolio_item.save
-        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now life.' }
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
       else
         format.html { render :new }
       end
