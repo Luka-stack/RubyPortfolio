f9d8183a3e1a4c21f32844ce273a1370db33f7c8
takacchi <uchiha.takahiro@gmail.com>
2019-03-04 17:47:24 +0100

    updated source tracking feature

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,16 +2,18 @@ module ApplicationHelper
   def login_helper style = ''
     if current_user.is_a?(GuestUser)
       (link_to 'Register', new_user_registration_path, class: style) + ' '.html_safe +
-      (link_to 'Login', new_user_session_path, class: style)
+        (link_to 'Login', new_user_session_path, class: style)
     else
       link_to 'Logout', destroy_user_session_path, method: :delete, class: style
     end
   end
 
-  def source_helper(layout_name)
+  def source_helper styles
     if session[:source]
-      greeting = "Thanks for visiting me from #{session[:source]} and you are on #{layout_name} layout"
-      content_tag(:p, greeting, class: 'source-greeting')
+      greeting = "Thanks for visiting me from #{session[:source]}, " \
+                 "please feel free to #{link_to 'contact me', contact_path} " \
+                 "if you'd like to work together."
+      content_tag(:div, greeting.html_safe, class: styles)
     end
   end
 
@@ -50,7 +52,7 @@ module ApplicationHelper
 
     nav_items.each do |item|
       nav_links << "<#{tag_type}><a href='#{item[:url]}'" +
-          "class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
+                   "class='#{style} #{active? item[:url]}'>#{item[:title]}</a></#{tag_type}>"
     end
 
     nav_links.html_safe
